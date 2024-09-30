c44f49217536f2ff220c9d768be6611a4768111c
Daiana <daiana.dominikow@gmail.com>
2024-09-30 18:40:32 -0300

    add pyserial

--- U9 - Parte I - leer datos Arduino/fotoresistor/fotoresistor_arduino.ino
+++ U9 - Parte I - leer datos Arduino/fotoresistor/fotoresistor_arduino.ino
@@ -1,5 +1,5 @@
 int led = 6;
-int foto = A0;
+int fotores = A0;
 
 void setup() {
 pinMode(led,OUTPUT);
@@ -10,8 +10,8 @@ delay(1000); // segundo
 void loop() {
   delay(1000); // un dato por segundo
   //leer la info del fotoresistor:
-  foto = analogRead(foto);
-  //la función MAP, hace el trabajo de pasar una escala (0 a 1023) a otra ( 0 a 255 )
+  foto = analogRead(fotores);
+  //la función MAP, hace el trabajo de pasar una escala de 10 bits (0 a 1023) a la de 8 bits del pin PWM (0 a 255)
   analogWrite(led,map(foto, 0, 1023, 0, 255));
   //para leer el valor del fotoresistor:
   Serial.println(foto);
