37c42c9c1d6e4d298865f2974ba0f46c3cfec395
Daiana <daiana.dominikow@gmail.com>
2024-10-04 22:43:00 -0300

    ordeno U9

--- U9 - PySerial - obtención data sensores/U9 - Parte I - leer datos Arduino/fotoresistor/fotoresistor_arduino.ino
+++ U9 - PySerial - obtención data sensores/U9 - Parte I - leer datos Arduino/fotoresistor/fotoresistor_arduino.ino
@@ -1,5 +1,8 @@
 int led = 6;
 int fotores = A0;
+int dato; 
+int datomap;
+
 
 void setup() {
 pinMode(led,OUTPUT);
@@ -10,10 +13,10 @@ delay(1000); // segundo
 void loop() {
   delay(1000); // un dato por segundo
   //leer la info del fotoresistor:
-  foto = analogRead(fotores);
+  dato = analogRead(fotores);
   //la función MAP, hace el trabajo de pasar una escala de 10 bits (0 a 1023) a la de 8 bits del pin PWM (0 a 255)
-  fotomap = map(foto, 0, 1023, 0, 255);
-  analogWrite(led,fotomap);
+  datomap = map(dato, 0, 1023, 0, 255);
+  analogWrite(led,datomap);
   //para leer el valor del fotoresistor:
-  Serial.println(foto);
+  Serial.println(dato);
 }
